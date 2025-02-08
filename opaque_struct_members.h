6bb8cd684d9c187e2c8dfeaa38ddcbb6239230cf
Kirandeep-Singh-Khehra <kirandeepsinghkhehra@gmail.com>
2025-02-08 12:56:19 +0530

    [opaque-struct-members] - Updated to consider only 64bit OS struct alignment
    Signed-off-by: Kirandeep-Singh-Khehra <kirandeepsinghkhehra@gmail.com>

--- opaque_struct_members.h
+++ opaque_struct_members.h
@@ -1,6 +1,6 @@
 #ifdef USE_PRIVATE_SCOPE
-  #define PRIVATE(STRUCT_PRIVATE_MEMBERS) struct { STRUCT_PRIVATE_MEMBERS };
+  #define PRIVATE(STRUCT_PRIVATE_MEMBERS) long: 64; struct { STRUCT_PRIVATE_MEMBERS };
 #else
-  #define PRIVATE(STRUCT_PRIVATE_MEMBERS) char _[sizeof(struct { STRUCT_PRIVATE_MEMBERS })];
+  #define PRIVATE(STRUCT_PRIVATE_MEMBERS) long: 64; char _[sizeof(struct { /*long: 64;*/ STRUCT_PRIVATE_MEMBERS }) /*- 8*/];
 #endif
 
